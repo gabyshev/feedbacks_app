4ceb5eb00e8f75c2776d6f733b6cfff6b19ce8c3
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-07 18:39:06 +0600

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'valid' do
+      let(:user) { build :user }
+      it { expect(user).to be_valid }
+    end
+
+    context 'invalid' do
+      let(:user) { build :user, email: '' }
+      it { expect(user).to_not be_valid }
+    end
+
+    context 'email' do
+      let!(:user) { create :user, email: 'email@test.com' }
+      let(:new_user) { build :user, email: 'email@test.com' }
+
+      it 'should be unique' do
+        expect(new_user.save).to be_falsey
+      end
+
+      it 'should say error message' do
+        expect(new_user.valid?).to be_falsey
+        expect(
+          new_user.errors.full_messages_for(:email)
+        ).to eq ['Email has already been taken']
+      end
+    end
+  end
 end
