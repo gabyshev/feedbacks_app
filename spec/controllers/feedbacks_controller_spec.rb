87720180690c339c7d95f7fc296cc3950d22762d
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-07 20:20:18 +0600

    FeedbacksController#create tests

--- spec/controllers/feedbacks_controller_spec.rb
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe FeedbacksController, type: :controller do
   describe '.index' do
-    context 'anonymous' do
+    context 'anonymous user' do
       it 'should be success' do
         get :index
         expect(response).to be_success
       end
     end
 
-    context 'sighned user' do
+    context 'logged user' do
       let(:user) { create :user }
 
       before do
@@ -19,6 +19,30 @@ RSpec.describe FeedbacksController, type: :controller do
 
       it 'should be success' do
         expect(response).to be_success
+      end
+    end
+  end
+
+  describe '.create' do
+    context 'valid params' do
+      let(:feedback_params) do
+        { feedback: { name: 'name', message: 'message' } }
+      end
+
+      it 'should redirect feedbacks page' do
+        post :create, feedback_params
+        expect(response.code).to eq '302'
+      end
+    end
+
+    context 'invalid params' do
+      let(:feedback_params) do
+        { feedback: { name: '', message: '' } }
+      end
+
+      it 'should redirect feedbacks page' do
+        post :create, feedback_params
+        expect(response.code).to eq '400'
       end
     end
   end
