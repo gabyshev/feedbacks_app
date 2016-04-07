67b0e736dc29694a923cbc3cb6f2758e287b9a30
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-07 19:50:54 +0600

    users name should be present

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,21 @@ RSpec.describe User, type: :model do
     end
 
     context 'invalid' do
-      let(:user) { build :user, email: '' }
-      it { expect(user).to_not be_valid }
+      let(:user) { build :user, email: '', name: '' }
+
+      it 'should have email' do
+        expect(user).to_not be_valid
+        expect(
+          user.errors.full_messages_for(:email)
+        ).to eq ["Email can't be blank"]
+      end
+
+      it 'should have name' do
+        expect(user).to_not be_valid
+        expect(
+          user.errors.full_messages_for(:name)
+        ).to eq ["Name can't be blank"]
+      end
     end
 
     context 'email' do
