91ff83cd954392e114d2fbf183a40ec98eeb643c
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-07 20:25:30 +0600

    feedbacks index and create actions

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,2 +1,21 @@
 class FeedbacksController < ApplicationController
+  def index
+    name = user_signed_in? ? current_user.name : nil
+    @feedback = Feedback.new(name: name)
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.valid?
+      redirect_to root_url, notice: 'Ok'
+    else
+      render :index, status: 400
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:name, :message)
+  end
 end
