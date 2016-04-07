5eba627771911f47e3e31416624e8bd7cb52ca07
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-07 19:18:10 +0600

    test FeedbacksController#index

--- spec/controllers/feedbacks_controller_spec.rb
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe FeedbacksController, type: :controller do
+  describe '.index' do
+    context 'anonymous' do
+      it 'should be success' do
+        get :index
+        expect(response).to be_success
+      end
+    end
 
+    context 'sighned user' do
+      let(:user) { create :user }
+
+      before do
+        sign_in user
+        get :index
+      end
+
+      it 'should be success' do
+        expect(response).to be_success
+      end
+    end
+  end
 end
